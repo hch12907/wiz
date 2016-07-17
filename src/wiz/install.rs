8e1c47c86ecedb5b1efdee64fa24425d2964e1bc
hch12907 <haochengho12907@gmail.com>
2016-07-17 19:28:19 +0800

    Protyping the package installation

---
+++ src/wiz/install.rs
@@ -0,0 +1,36 @@
+fn find_package(name: &str)
+{
+    /*
+      This function checks the availablity of the
+      package and returns boolean indicating that.
+    */ 
+}
+
+fn verify_package(package: &File, provided: &str)
+{
+    /*
+      This function verifies the package(SHA-1 or MD5 perhaps?)
+      and returns boolean indicating that.
+    */
+}
+
+fn copy_package()
+{
+    /*
+      This function retrieves the package, and put them
+      to specified locations accordingly.
+    */
+}
+
+fn process_package()
+{
+    /*
+      This function finalises the installation of
+      the package, such as setting PATH and such.
+    */
+}
+
+fn install()
+{
+
+}
