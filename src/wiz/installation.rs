a1803008b6d476f6ec1b6aba36dad8428f269be8
hch12907 <haochengho12907@gmail.com>
2016-08-21 02:00:22 +0800

    find_package() completed

--- src/wiz/installation.rs
+++ src/wiz/installation.rs
@@ -1,11 +1,43 @@
-use std::fs::File;
+use constants;
 
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::path::Path;
+
+
+// TODO: Eliminate hardcoded strings and ints
 fn find_package(name: &str) -> String
 {
-    
+    let mut list = match File::open(constants::PACKAGE_LIST) {
+        Err(why) => panic!("An error occured:\n{}", why),
+        Ok(file) => file,
+    };
+
+    let mut reader = BufReader::new(list);
+    let mut buffer = String::new();
+
+    for line in reader.lines() {
+        let x = line.unwrap();
+        let tokens: Vec<&str> = x.split(' ').collect();
+
+        if tokens.len() < 2 {
+            buffer.push_str("NOT FOUND");
+        }
+        else {   
+            if tokens[0] == name {
+                buffer.push_str(tokens[1]);
+            } 
+            else {
+                buffer.push_str("NOT FOUND"); // this is only temporary!
+            }
+        }
+    }
+
+    buffer
 }
 
-fn retrieve_package(url: &str) -> Path
+fn retrieve_package(url: &str) // -> Path
 {
     /*
       This function retrieves the package, and
