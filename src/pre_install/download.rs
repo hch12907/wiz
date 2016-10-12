0124e8bc03b57609f45190d23291766766bb6e0c
hch12907 <haochengho12907@gmail.com>
2016-10-12 22:25:34 +0800

    Basic functionality of downloading

--- src/pre_install/download.rs
+++ src/pre_install/download.rs
@@ -3,3 +3,20 @@
     Write code for downloading the package, into a
     specific path.
 */
+crate hyper;
+use self::hyper::client;
+
+macro_rules! custom_try {
+    ($x:expr) => (match $x {
+        Ok(x) => x,
+        Err(why) => panic!("An error occured during package downloading.", why),
+    });
+}
+
+fn download_package(url: &str) {
+    let client = Client::new();
+    let response = custom_try!(client.get(url).send());
+    let content = Vec::new(); 
+    custom_try!(response.read_to_end(&mut content));
+    return content;
+}
