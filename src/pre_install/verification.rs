93aa5e7d7874bece916b5f3944b1d6e5b95989a5
hch12907 <haochengho12907@gmail.com>
2016-10-12 22:32:40 +0800

    Complete file verification functionality.

---
+++ src/pre_install/verification.rs
@@ -0,0 +1,18 @@
+extern crate crc;
+
+use std::io::{ BufRead, BufReader, Read };
+use std::fs::File;
+use std::path::Path;
+
+use self::crc::{crc32, Hasher32};
+
+pub fn verify_file_crc32(file: &File) -> u32
+{
+    let mut digest = crc32::Digest::new(crc32::IEEE);
+
+    for line in file.bytes() {
+        digest.write(&[line.unwrap()]);
+    }
+
+    digest.sum32()
+}
