6bc1c69b61b675eba85f246e6dc4a65ab9cb0bd0
hch12907 <haochengho12907@gmail.com>
2016-11-13 13:29:49 +0800

    Switch from hyper to reqwest

--- src/download.rs
+++ src/download.rs
@@ -4,9 +4,9 @@
     2. Verify the downloaded package.
     3. Make it support repos someday.
 */
-extern crate hyper;
+extern crate reqwest;
 
-use self::hyper::Client;
+use self::reqwest::Client;
 use std::fs::File;
 use std::io::{ Read, Write };
 use std::path::Path;
@@ -19,8 +19,7 @@ macro_rules! custom_try {
 }
 
 pub fn download_file(url: &str, output: &Path) {
-    let client = Client::new();
-    let response = custom_try!(client.get(url).send());
+    let response = custom_try!(reqwest::get(url));
     let mut target = custom_try!(File::create(output));
 
     for byte in response.bytes() {
