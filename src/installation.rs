874562db091b5c3534dca899a2178056033a7d10
hch12907 <haochengho12907@gmail.com>
2016-11-18 17:41:10 +0800

    Package installation complete

--- src/installation.rs
+++ src/installation.rs
@@ -7,15 +7,29 @@ use verify;
 use find_package::Package;
 
 fn _install_package(package: Package, path: &Path) -> Result<bool, String> {
+    let download_path = PathBuf::new();
+        download_path.push(path);
+        download_path.push(format!("download/{}", package.name));
+
+    download::download_file(package.url, download_path);
+
+    let tar_path = PathBuf::new();
+        tar_path.push(path);
+        tar_path.push(format!("download/{}.tar", package.name));
+
+    extract::extract_gz(download_path, tar_path);
+
     let install_path = PathBuf::new();
-        install_path.push(path);
-        install_path.push(format!("installed/{}", package.name));
+        tar_path.push(path);
+        tar_path.push(format!("install/{}", package.name));
+
+    extract::extract_tar(tar_path, install_path);
 }
 
 pub fn install_package(name: &str, path: &Path) -> Result<bool, String> {
     let found_packages = try!(find_package::find_package(name, path));
     
-    if(found_packages.count() > 1) {
+    if(!found_packages.is_empty() && found_packages.count() > 1) {
         for (i, package) in found_packages.iter().enumerate() {
             let install_path = PathBuf::new();
                 install_path.push(path);
@@ -28,6 +42,6 @@ pub fn install_package(name: &str, path: &Path) -> Result<bool, String> {
 
         //TODO: Accept user input and install user-chosen package.
     } else {
-        return _install_package(package)
+        return _install_package(found_packages[0], path)
     }
 }
