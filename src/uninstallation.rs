2dc4eed66e2219f40a999893eb23ef4b20873982
hch12907 <haochengho12907@gmail.com>
2016-11-20 16:36:13 +0800

    Full uninstallation functionality

--- src/uninstallation.rs
+++ src/uninstallation.rs
@@ -5,10 +5,10 @@ use find_package;
 
 fn uninstall(name: &str, path: &Path) -> Result<bool, String> {
     let package = find_package::select_package(name, path);
+    
     let mut installation_path = PathBuf::new();
-
-    installation_path.push(path);
-    installation_path.push(format!("install/{}", package.name));
+            installation_path.push(path);
+            installation_path.push(format!("install/{}", package.name));
 
     if installation_path.exists() {
         fs::remove_dir_all(&path)?;
@@ -16,4 +16,20 @@ fn uninstall(name: &str, path: &Path) -> Result<bool, String> {
     } else {
         return Ok(false)
     }
+}
+
+fn full_uninstall(name: &str, path: &Path) -> Result<bool, String> {
+    uninstall(name, path)?;
+
+    let mut download_path = PathBuf::new();
+            download_path.push(path);
+            download_path.push(format!("download/{}", package.name));
+    fs::remove_file(tar_path)?;
+
+    let mut tar_path = PathBuf::new();
+            tar_path.push(path);
+            tar_path.push(format!("download/{}.tar", package.name));
+    fs::remove_file(tar_path)?;
+
+    return Ok(true)
 }
