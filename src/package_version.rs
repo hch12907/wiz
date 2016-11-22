e06208613a1ca75b462f9b00638591d7f92ccfb5
hch12907 <haochengho12907@gmail.com>
2016-11-22 22:09:53 +0800

    Package version initialize 2

---
+++ src/package_version.rs
@@ -0,0 +1,9 @@
+fn get_version() -> Result<String, String> {
+    //TODO:
+    //  Gets the version of the package
+}
+
+fn put_version() -> Result<bool, String> {
+    //TODO:
+    // Sets/Updates the version of the package
+}
