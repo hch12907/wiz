af8fe7fe69d0d6d952e32db52fb5d97af3d8735c
hch12907 <haochengho12907@gmail.com>
2016-11-24 16:39:29 +0800

    Basic function for parsing package version list.

--- src/package_version.rs
+++ src/package_version.rs
@@ -1,9 +1,40 @@
-fn get_version() -> Result<String, String> {
-    //TODO:
-    //  Gets the version of the package
+use std::fs::File;
+use std::io::{ BufRead, BufReader };
+use std::path::Path;
+
+fn parse_list(path: &Path) -> Result<Vec<(String, u32)>, String> {
+    let list = match File::open(path) {
+        Ok(x) => x,
+        Err(why) => return Err(format!("{}", why))
+    };
+    let list = BufReader::new(list);
+    
+    let parsed_packages = Vec::new(); 
+    for line in list.lines() {
+        let split: Vec<&str> = line.unwrap_or(String::from("")).trim().split('=').collect();
+        let package_name = String::from(split[0]);
+        let package_version = split[1].parse::<u32>().unwrap_or(0); // For this reason, version of packages MUST NOT BE 0
+
+        if !package_name.is_empty() || package_version != 0 {
+            parsed_packages.push((package_name, package_version));
+        }
+    }
+
+    if parsed_packages.len() > 0 {
+        return Ok(parsed_packages)
+    } else {
+        return Err(String::from("No package found."))
+    }
 }
 
-fn put_version() -> Result<bool, String> {
+fn get_version(name: &str, path: &Path) -> Option<u32> {
+    //TODO:
+    //  Gets the version of the package
+    
+
+}
+
+fn put_version() -> Result<bool, String>{
     //TODO:
     // Sets/Updates the version of the package
 }
