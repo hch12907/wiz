6de8c7ad42533ca5edc39b4bb702ea6c9c06edfd
hch12907 <haochengho12907@gmail.com>
2016-12-02 18:44:09 +0800

    Implement PartialEq for Package and Version

--- src/package/package.rs
+++ src/package/package.rs
@@ -11,12 +11,30 @@ pub struct Package {
     pub name: String,
     pub version: Version,
     pub crc32: u32,
-    pub url: String,
+    pub child_url: String, // Examples: /some_dir/something.exe
     pub dependencies: Vec<Package> 
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
 pub struct PackageList {
     packages: Vec<Package>,
-    version: i32
+    parent_url: String, // Examples: https://www.foo.bar
+    version: u32
+}
+
+impl PartialEq for Version {
+    fn eq(&self, other: &Version) -> bool {
+        self.major == other.major &&
+        self.minor == other.minor &&
+        self.patch == other.patch
+    }
+}
+
+impl PartialEq for Package {
+    fn eq(&self, other: &Package) -> bool {
+        self.name == other.name &&
+        self.version == other.version &&
+        self.crc32 == other.crc32 &&
+        self.dependencies == other.dependencies
+    }
 }
