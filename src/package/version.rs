18519d323576dc59816097216547b798c6f5cdb5
hch12907 <haochengho12907@gmail.com>
2016-12-11 20:17:06 +0800

    Switch to use JSON to save installed package versions

--- src/package/version.rs
+++ src/package/version.rs
@@ -1,31 +1,37 @@
+use rustc_serialize::{ json, Encodable };
+
 use std::error::Error;
 use std::fs::File;
-use std::io::{ BufRead, BufReader };
+use std::io::{ Read, Write };
 use std::path::Path;
 
 use package::pkg::{ Package, Version };
 
-const SEPERATION_SYMBOL: &'static str = "=";
-
-pub fn get_installed_version(pkg: Package, path: &Path) -> Result<Version, String> {
-    let file = get!(File::open(path), "An error occured while opening version list.");
-    let file = BufReader::new(file);
+pub fn get_list(path: &Path) -> Result<Vec<(String, Version)>, String> {
+    let mut buffer = String::new();
+    let mut file = get!(File::open(path), "An error occured while opening file");
     
-    for line in file.lines() {
-        let line: String = get!(line, "An error occured while trying to unwrap line.");
-        if line.contains(&pkg.name) {
-            let ver: Vec<&str> = line.trim().split(SEPERATION_SYMBOL).collect();
-            if ver.len() == 2 {
-                return Ok(try!(Version::from_str(ver[1])))
-            } else {
-                return Err("Wrong version".to_string())
-            }
-        }
-    }
-
-    return Err("Specified package not found".to_string())
+    get!(file.read_to_string(&mut buffer), "An error occured while reading file");
+    return Ok(get!(json::decode(&buffer), "An error occured while decoding JSON"))
 }
 
-pub fn update_installed_version(pkg: Package, path: &Path, version: Version) -> Result<bool, String> {
-    
+fn raw_update<T: Encodable>(object: &T, path: &Path) -> Result<(), String> {
+    let mut file = get!(File::open(path), "An error occured while opening file");
+    return match file.write(get!(json::encode(&object), "An error occured while encoding list").as_bytes()) {
+        Ok(_) => Ok(()),
+        Err(why) => Err(why.description().to_string())
+    }
+}
+
+pub fn update_list(package: &Package, path: &Path) -> Result<(), String> {
+    let mut list: Vec<(String, Version)> = try!(get_list(path));
+    for x in 0..(list.len() - 1) {
+        if &list[x].0 == &package.name {
+            list[x].1 = package.version.clone();
+            return raw_update(&list, path)
+        }
+    };
+
+    list.push((package.name.clone(), package.version.clone()));
+    return raw_update(&list, path)
 }
