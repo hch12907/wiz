9343966a4d41df14c4abca65ae6fc8c0ad09e8db
hch12907 <haochengho12907@gmail.com>
2016-12-13 20:00:12 +0800

    Documentation for download.rs

--- src/backend/download.rs
+++ src/backend/download.rs
@@ -6,13 +6,19 @@ use std::path::Path;
 use reqwest;
 use reqwest::header::ContentLength;
 
+/// Downloads file from specified URL, and output it
+/// to the path specified in `output`. While downloading
+/// the file, for each byte downloaded, it invokes `and`.
+/// When the download is complete, it returns a Result that
+/// returns file size if download succeeds, or returns error
+/// message if download fails. 
 pub fn download_file_while<F>(url: &str, output: &Path, and: F) -> Result<u64, String> 
     where F: Fn(u64, &u64) {
         
     // Abandoning this code, as the filename is going to be unpredictable in this case.
-    /*let mut output_to = PathBuf::new();
-            output_to.push(output);
-            output_to.push(url.split('/').last().unwrap_or("_error_"));*/ 
+    //let mut output_to = PathBuf::new();
+    //        output_to.push(output);
+    //        output_to.push(url.split('/').last().unwrap_or("_error_"));*/ 
 
     let response = get!(reqwest::get(url), "Error occured while making a GET request. Reason:\n");
     let target = get!(File::create(output), "Error occured while creating file. Reason:\n");
@@ -33,6 +39,12 @@ pub fn download_file_while<F>(url: &str, output: &Path, and: F) -> Result<u64, S
     Ok(length)
 }
 
+/// Downloads file from specified URL, and output it
+/// to the path specified in `output`. After the file is
+/// downloaded, it invokes `and`.
+/// When the download is complete, it returns a Result that
+/// returns file size if download succeeds, or returns error
+/// message if download fails. 
 pub fn download_file_and<F>(url: &str, output: &Path, and: F) -> Result<u64, String> 
     where F: Fn(u64) {
     let length = try!(download_file_while(url, output, |_, _| { }));
@@ -40,6 +52,11 @@ pub fn download_file_and<F>(url: &str, output: &Path, and: F) -> Result<u64, Str
     Ok(length)
 }
 
+/// Downloads file from specified URL, and output it to
+/// the path specified in `output`.
+/// When the download is complete, it returns a Result that
+/// returns file size if download succeeds, or returns error
+/// message if download fails. 
 pub fn download_file(url: &str, output: &Path) -> Result<u64, String> {
     download_file_and(url, output, |_| { }) // Do nothing for and. 
 }
