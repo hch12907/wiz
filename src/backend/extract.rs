069d60e7faeff8c234688a5da2503667ff77c303
hch12907 <haochengho12907@gmail.com>
2016-12-13 20:05:41 +0800

    Documentation for extract.rs

--- src/backend/extract.rs
+++ src/backend/extract.rs
@@ -6,6 +6,10 @@ use std::path::Path;
 use flate2::read::GzDecoder;
 use tar::Archive;
 
+/// Extracts the tarball found in the path specified in `input`,
+/// and outputs it into path specified in `output`.
+/// If the extract process failed, it returns a String containing
+/// the error message. 
 pub fn extract_tar(input: &Path, output: &Path) -> Result<(), String> {
     let file = get!(File::open(input), "An error occured while opening file. (extract_tar)");
     let mut archive = Archive::new(file);
@@ -13,6 +17,10 @@ pub fn extract_tar(input: &Path, output: &Path) -> Result<(), String> {
     return Ok(())
 }
 
+/// Extracts the gzip found in the path specified in `input`,
+/// and outputs it into path specified in `output`.
+/// If the extract process failed, it returns a String containing
+/// the error message. 
 pub fn extract_gz(input: &Path, output: &Path) -> Result<(), String> {
     let file = get!(File::open(input), "An error occured while opening file. (extract_gz) Reason:\n");
     let archive = get!(GzDecoder::new(BufReader::new(file)), "An error occured while creating GzDecoder. Reason:\n");
