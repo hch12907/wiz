48b351f6a51ccdae03bb246622c7691769f7307f
hch12907 <haochengho12907@gmail.com>
2017-11-20 17:49:53 +0800

    An early implementation of Package.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod package;
+
 fn main() {
     println!("Hello world!");
 }
---
+++ src/package.rs
@@ -0,0 +1,25 @@
+enum PackageType {
+    Bin, // Binary
+    Lib, // Library
+    Src, // Source
+}
+
+struct Version {
+    major: u32,
+    minor: u32,
+    patch: u32,
+    additional: Option<String>,
+}
+
+struct Package {
+    name: String,
+    hash: u32,
+    version: Version,
+    package_type: PackageType,
+    dependencies: Vec<Package>, 
+}
+
+struct List {
+    url: String,
+    packages: Vec<Package>,
+}
