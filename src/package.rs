3208fc0d966e2bdd69e931e3153eb2c010c431cc
hch12907 <haochengho12907@gmail.com>
2017-11-20 18:23:16 +0800

    Allow comparison between `Version`s

--- src/package.rs
+++ src/package.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 enum PackageType {
     Bin, // Binary
     Lib, // Library
@@ -22,4 +24,38 @@ struct Package {
 struct Repository {
     url: String,
     packages: Vec<Package>,
+}
+
+impl PartialEq for Version {
+    fn eq(&self, other: &Self) -> bool {
+        self == other
+    }
+}
+
+impl Eq for Version {}
+
+impl PartialOrd for Version {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Version {
+    /// This compares one `Version` with another. It only compares the
+    /// three main parts of Version (`major`, `minor`, `patch`) and ignores
+    /// `additional` as it is meant to let the package host to put additional
+    /// messages such as `alpha`, `beta`.
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.major > other.major { Ordering::Greater } else
+        if self.major < other.major { Ordering::Less } 
+        else {
+            if self.minor > other.minor { Ordering::Greater } else
+            if self.minor < other.minor { Ordering::Less }
+            else {
+                if self.patch > other.patch { Ordering::Greater } else
+                if self.patch < other.patch { Ordering::Less } 
+                else { Ordering::Equal }
+            }
+        }
+    }
 }
