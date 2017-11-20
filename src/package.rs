a70ebc4d75a7faf6277fa68a8bcc879a087dfe8e
hch12907 <haochengho12907@gmail.com>
2017-11-20 19:08:47 +0800

    Finishing touches on Version

--- src/package.rs
+++ src/package.rs
@@ -6,7 +6,18 @@ enum PackageType {
     Src, // Source
 }
 
-struct Version {
+/// A struct made for storing the version of the packages.
+/// 
+/// The format follows [semver 2.0](http://semver.org/), meaning that:
+/// 
+/// - versions with different `major` indicates that they are incompatible.
+/// - versions with different `minor` but same `major` are largely compatible.
+/// - versions with different `patch` but same `major` and `minor` are 
+///   compatible.
+/// 
+/// The `additional` field is an optional field for the package host to put
+/// text such as `alpha` or `stable`, it's not involved in the comparisons.
+pub struct Version {
     major: u32,
     minor: u32,
     patch: u32,
@@ -45,6 +56,10 @@ impl Ord for Version {
     /// three main parts of Version (`major`, `minor`, `patch`) and ignores
     /// `additional` as it is meant to let the package host to put additional
     /// messages such as `alpha`, `beta`.
+    /// 
+    /// This functions compares the two `Version`s *absolutely*, meaning that
+    /// it doesn't care about whether the two versions are breaking-changes or
+    /// not - it just compares.
     fn cmp(&self, other: &Self) -> Ordering {
         if self.major > other.major { Ordering::Greater } else
         if self.major < other.major { Ordering::Less } 
@@ -57,5 +72,33 @@ impl Ord for Version {
                 else { Ordering::Equal }
             }
         }
+    }
+}
+
+impl Version {
+    /// Returns the major version X (X.y.z).
+    pub fn major(&self) -> u32 {
+        self.major
+    }
+
+    /// Returns the minor version Y (x.Y.z).
+    pub fn minor(&self) -> u32 {
+        self.minor
+    }
+
+    /// Returns the patch version Z (x.y.Z).
+    pub fn patch(&self) -> u32 {
+        self.patch
+    }
+
+    /// Returns the additional message added by package host or its developers.
+    pub fn additional(&self) -> &Option<String> {
+        &self.additional
+    }
+
+    /// A helper function for checking whether the two versions contains
+    /// breaking changes. 
+    pub fn is_back_compatible(&self, other: &Self) -> bool {
+        self.major == other.major
     }
 }
