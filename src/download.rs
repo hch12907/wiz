9d1afd861f2e81c053427554ab9ddae4af26cfc5
hch12907 <haochengho12907@gmail.com>
2017-11-21 18:40:39 +0800

    Adding a new state of the pkg download process

--- src/download.rs
+++ src/download.rs
@@ -2,20 +2,45 @@ use std::path::Path;
 
 use error::PackageError;
 
-enum PackageDownload<'a> {
-    HasDownloaded(&'a Path),
-    YetDownloaded(&'a str)
+/// Enum for representing the state of the package downloads.
+pub enum PackageDownload<'a> {
+    /// The state is switched to IsFinished when the download of the
+    /// package is finished, leaving only a `&Path` which is the location
+    /// of the downloaded package.
+    IsFinished(&'a Path),
+    /// The state is switched to InProgress when the download of the
+    /// package is started, the `&Path` is the location where the
+    /// half-downloaded package is located at, while the `u32` is the progress
+    /// of the current download process.
+    InProgress(&'a Path, u32),
+    /// This is the initial state of PackageDownload, where the package is yet
+    /// to be downloaded. It has a `&str` for storing the URL, and a `&Path`
+    /// of which the package is going to be located at, when the package is
+    /// downloaded.
+    NotStarted(&'a str, &'a Path)
 }
 
 impl<'a> PackageDownload<'a> {
-    fn from_url(url: &'a str) -> Self {
-        PackageDownload::YetDownloaded(url)
+    /// Create a new PackageDownload.
+    fn new(url: &'a str, path: &'a Path) -> Self {
+        PackageDownload::NotStarted(url, path)
     }
 
+    /// A bool to determine whether the package is downloaded.
     fn is_downloaded(&self) -> bool {
         match self {
-            &PackageDownload::HasDownloaded(_) => true,
+            &PackageDownload::IsFinished(_) => true,
             _ => false
+        }
+    }
+
+    /// Returns the path of where the package is (or is going to be) located
+    /// at.
+    fn store_path(&self) -> &'a Path {
+        match self {
+            &PackageDownload::IsFinished(ref path) => path,
+            &PackageDownload::InProgress(ref path, _) => path,
+            &PackageDownload::NotStarted(_, ref path) => path,
         }
     }
 }
