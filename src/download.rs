bf58bcb9aa444e4b8f1855571761f7acabdadcb5
hch12907 <haochengho12907@gmail.com>
2017-11-21 21:47:55 +0800

    Use struct-variant instead of tuple-variant

--- src/download.rs
+++ src/download.rs
@@ -5,32 +5,45 @@ use error::PackageError;
 /// Enum for representing the state of the package downloads.
 pub enum PackageDownload<'a> {
     /// The state is switched to IsFinished when the download of the
-    /// package is finished, leaving a `&Path` which is the location
-    /// of the downloaded package, and a `u32` which is the file size.
-    IsFinished(&'a Path, u32),
+    /// package is finished.
+    IsFinished {
+        /// The path where the downloaded package is located at.
+        location: &'a Path,
+        /// The total file size of the package.
+        file_size: u32,
+    },
+
     /// The state is switched to InProgress when the download of the
-    /// package is started, the `&Path` is the location where the
-    /// half-downloaded package is located at, while the first `u32` is the
-    /// progress of the current download process in bytes, the third `u32`
-    /// is the total file size of the package.
-    InProgress(&'a Path, u32, u32),
+    /// package is started.
+    InProgress {
+        /// The path where the half-downloaded package is located at.
+        location: &'a Path,
+        /// The progress of the current download process.
+        progress: u32,
+        /// The total file size of the package.
+        file_size: u32,
+    },
+
     /// This is the initial state of PackageDownload, where the package is yet
-    /// to be downloaded. It has a `&str` for storing the URL, and a `&Path`
-    /// of which the package is going to be located at, when the package is
-    /// downloaded.
-    NotStarted(&'a str, &'a Path)
+    /// to be downloaded.
+    NotStarted {
+        /// The path where the package is going to be located at.
+        location: &'a Path,
+        /// The URL of where the package is going to be downloaded from.
+        url: &'a str,
+    }
 }
 
 impl<'a> PackageDownload<'a> {
     /// Create a new PackageDownload.
-    fn new(url: &'a str, path: &'a Path) -> Self {
-        PackageDownload::NotStarted(url, path)
+    fn new(url: &'a str, location: &'a Path) -> Self {
+        PackageDownload::NotStarted { location, url }
     }
 
     /// A bool to determine whether the package is downloaded.
     fn is_downloaded(&self) -> bool {
         match self {
-            &PackageDownload::IsFinished(_,_) => true,
+            &PackageDownload::IsFinished { location: _, file_size: _ } => true,
             _ => false
         }
     }
@@ -39,9 +52,9 @@ impl<'a> PackageDownload<'a> {
     /// at.
     fn store_path(&self) -> &'a Path {
         match self {
-            &PackageDownload::IsFinished(ref path, _) => path,
-            &PackageDownload::InProgress(ref path, _, _) => path,
-            &PackageDownload::NotStarted(_, ref path) => path,
+            &PackageDownload::IsFinished { location: path, file_size: _ } => path,
+            &PackageDownload::InProgress { location: path, progress: _ , file_size: _} => path,
+            &PackageDownload::NotStarted { location: path, url: _ } => path,
         }
     }
 }
