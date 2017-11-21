f944b7c7093f317ba48e05df6a03df05942ed68d
hch12907 <haochengho12907@gmail.com>
2017-11-21 13:40:42 +0800

    Renaming Error to PackageError and bugfixes

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,17 @@
+use std::error::Error;
 use std::io;
 
 /// The error type for the program's every operations, such as TOML Parsing
 /// or IO-related errors. Wherever there's an error, it _should_ be wrapped
 /// into this type.
-pub enum Error {
+pub enum PackageError {
     Parsing(String),
     IO(String),
 }
 
-impl From<io::Error> for Error {
+impl From<io::Error> for PackageError {
     /// Converts an `io::Error` into this type.
     fn from(err: io::Error) -> Self {
-        Error::IO(String::from("An IO error occured:") + err.description())
+        PackageError::IO(String::from("An IO error occured:") + err.description())
     }
 }
