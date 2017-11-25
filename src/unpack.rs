850537675d4eed3be802526bd14e8de75ac6e701
hch12907 <haochengho12907@gmail.com>
2017-11-25 23:17:50 +0800

    Functions for extracting tarballs

--- src/unpack.rs
+++ src/unpack.rs
@@ -1,7 +1,16 @@
-fn unpack_tar() {
+use error::PackageError;
+use std::fs::{ File, metadata };
+use std::path::Path;
+use tar::Archive;
 
+fn unpack_tar(input: &Path, target: &Path) -> Result<u64, PackageError> {
+    let file = File::open(input)?;
+    let mut archive = Archive::new(file);
+    archive.unpack(target)?;
+    let target_file = metadata(target)?;
+    Ok(target_file.len())
 }
 
 fn unpack_gzip() {
-    
+
 }
