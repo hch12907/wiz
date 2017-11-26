2071a1382f05b4c81ff6494a72f042f96fccc827
hch12907 <haochengho12907@gmail.com>
2017-11-26 19:47:08 +0800

    Adding defaults for the Config

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,24 @@
 // In heavy WIP
+use std::fs::File;
+use std::io::Read;
+use toml;
+
+use error::PackageError;
 
 #[derive(Deserialize)]
 struct Config {
-    download_path: String
+    buffer_size: Option<u64>,
+    config_path: Option<String>,
+    download_path: Option<String>,
 }
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            buffer_size: Some(65536),
+            config_path: Some(String::from("~/.wiz/config/")),
+            download_path: Some(String::from("~/.wiz/downloads/")),
+        }
+    }
+}
+
