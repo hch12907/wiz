971df2d15201bdc27aea5c0ed13176d70bdcc0f3
hch12907 <haochengho12907@gmail.com>
2017-11-27 18:00:53 +0800

    The buffer_size is now configurable

--- src/download.rs
+++ src/download.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io::{ BufWriter, Read, Write };
 use std::path::Path;
 
+use config::Config;
 use error::PackageError;
 
 fn download_file<F>(url: &str, to_path: &Path, callback: F) -> Result<u64, PackageError>
@@ -23,8 +24,12 @@ fn download_file<F>(url: &str, to_path: &Path, callback: F) -> Result<u64, Packa
         });
 
 
-    // For every `BUFFER_SIZE` bytes, call the callback once.
-    const BUFFER_SIZE: u64 = 65536;
+    // For every `buffer_size` bytes, call the callback once.
+    let Config { buffer_size: buffer_size, .. } = Config::default();
+    
+    // `buffer_size` decides how frequent the function calls the callback by
+    // checking whether (downloaded bytes) % `buffer_size` is 0.
+    let buffer_size = buffer_size.unwrap_or(65536);
 
     // Obviously, the target file to be written to by the BufWriter.
     let mut target = BufWriter::new(File::create(to_path)?);
@@ -35,7 +40,7 @@ fn download_file<F>(url: &str, to_path: &Path, callback: F) -> Result<u64, Packa
 
         // Checks whether it's time to call the callback, and call it if it is.
         // Plus one to `index` as `index` itself is zero-indexed.
-        if index as u64 % BUFFER_SIZE == 0 {
+        if index as u64 % buffer_size == 0 {
             callback((index + 1) as u64, content_length);
         };
     }
