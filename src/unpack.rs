bbc1662fee6ac31c2fd1d8f28bb472fb0f3342bc
hch12907 <haochengho12907@gmail.com>
2017-11-28 20:39:01 +0800

    Function for unpacking gzip.

--- src/unpack.rs
+++ src/unpack.rs
@@ -1,5 +1,7 @@
 use error::PackageError;
+use flate2::bufread::GzDecoder;
 use std::fs::{ File, metadata };
+use std::io::{ BufReader, BufWriter, Read, Write };
 use std::path::Path;
 use tar::Archive;
 
@@ -11,6 +13,15 @@ fn unpack_tar(input: &Path, target: &Path) -> Result<u64, PackageError> {
     Ok(target_file.len())
 }
 
-fn unpack_gzip() {
-
+fn unpack_gzip(input: &Path, target: &Path) -> Result<u64, PackageError> {
+    let file = File::open(input)?;
+    let decoder = GzDecoder::new(BufReader::new(file))?;
+    let target = File::create(target)?;
+    let mut target = BufWriter::new(target);
+    let mut bytes = 0;
+    for byte in decoder.bytes() { 
+        target.write(&[byte?])?;
+        bytes += 1;
+    }
+    Ok(bytes)
 }
