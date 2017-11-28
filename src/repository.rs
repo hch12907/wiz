aa83a393e1fde62d8197a78d8138192c4cec946f
hch12907 <haochengho12907@gmail.com>
2017-11-28 16:37:57 +0800

    Adding RepositoryList

--- src/repository.rs
+++ src/repository.rs
@@ -1,10 +1,21 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use toml::Value as Toml;
+
+use error::PackageError;
 use package::Package;
 
 /// The struct which represents the repos.
+#[derive(Deserialize)]
 pub struct Repository {
     url: String,
     packages: Vec<Package>,
 }
+
+/// Struct which represents a list of Repositories.
+#[derive(Deserialize)]
+struct RepositoryList(Vec<Repository>);
 
 impl Repository {
     /// Returns the URL of the Repository.
@@ -28,5 +39,16 @@ impl Repository {
         };
 
         None
+    }
+}
+
+impl RepositoryList {
+    fn read_from(path: &Path) -> Result<Self, PackageError> {
+        let mut list = File::open(path)?;
+        let mut content = String::new();
+        list.read_to_string(&mut content)?;
+        let list = content.parse::<Toml>()?;
+        let list = list.try_into::<Self>()?;
+        Ok(list)
     }
 }
