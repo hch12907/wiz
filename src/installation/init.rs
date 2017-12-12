af73aad3dcda8f53f793b40e3db78cd3f3914244
hch12907 <haochengho12907@gmail.com>
2017-12-12 20:55:30 +0800

    Starting works on init

--- src/installation/init.rs
+++ src/installation/init.rs
@@ -1,5 +1,16 @@
+use std::path::Path;
+
+use config::Config;
 use installation::PackageState;
 
-fn get_config() {
+/// Gets the config, fill it with default values, and return it.
+fn get_config<P: AsRef<Path>>(path: P) -> Config {
+    Config::read_from(path)
+        .map(|x| x.fill_with_default())
+        .unwrap()
+}
+
+/// Gets the cached repositories.
+fn get_cache<P: AsRef<Path>>(path: P) {
     
 }
