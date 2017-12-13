abdb649429da2dee80ec15c3bfb8cdb64b478f1d
hch12907 <haochengho12907@gmail.com>
2017-12-13 23:56:18 +0800

    Implement read_from for Cache

--- src/installation/cache.rs
+++ src/installation/cache.rs
@@ -1,8 +1,22 @@
-use utils::Hash;
-use installation::PackageState;
+use std::fs::File;
+use std::io::{ BufReader, Read };
+use std::path::Path;
+use toml::Value;
 
+use error::PackageError;
+use installation::PackageState;
+use utils::Hash;
+
+#[derive(Serialize, Deserialize)]
 pub struct Cache(Vec<(Hash, PackageState)>);
 
 impl Cache {
-
+    fn read_from<P: AsRef<Path>>(path: P) -> Result<Cache, PackageError> {
+        let mut file = File::open(path)?;
+        let mut file = BufReader::new(file);
+        let mut content = String::new();
+        file.read_to_string(&mut content);
+        let content = content.trim().parse::<Value>()?;
+        Ok(content.try_into::<Self>()?)
+    }   
 }
