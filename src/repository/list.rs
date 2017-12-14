3121437a7f3015839489b1717ef334d1bdd89654
hch12907 <haochengho12907@gmail.com>
2017-12-14 23:56:34 +0800

    Read/Write for RepositoryList.

--- src/repository/list.rs
+++ src/repository/list.rs
@@ -1,6 +1,30 @@
+use std::fs::File;
+use std::io::{ BufReader, BufWriter, Read, Write };
+use std::path::Path;
+use toml::{ to_string as toml_to_string, Value };
+
+use error::PackageError;
 use repository::Repository;
 
 /// Caches all the repositories into a single list, so they can be accessed
 /// everytime without downloading all of them again.
 #[derive(Serialize, Deserialize)]
 pub struct RepositoryList(pub Vec<Repository>);
+
+impl RepositoryList {
+    pub fn read_from<P: AsRef<Path>>(path: P) -> Result<Self, PackageError> {
+        let file = File::open(path)?;
+        let mut file = BufReader::new(file);
+        let mut content = String::new();
+        file.read_to_string(&mut content);
+        let content = content.trim().parse::<Value>()?;
+        Ok(content.try_into::<Self>()?)
+    }
+
+    pub fn write_to<P: AsRef<Path>>(&self, path: P) -> Result<(), PackageError> {
+        let file = File::create(path)?;
+        let mut file = BufWriter::new(file);
+        let content = toml_to_string(self)?;
+        Ok(file.write_all(content.as_bytes())?)
+    }
+}
