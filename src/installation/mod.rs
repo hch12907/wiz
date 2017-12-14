ec62fa81d1b44d47a2e2f71dc2ac462aca8502a8
hch12907 <haochengho12907@gmail.com>
2017-12-15 00:01:09 +0800

    Reexport init functions

--- src/installation/mod.rs
+++ src/installation/mod.rs
@@ -1,7 +1,10 @@
 mod cache;  
-pub mod init;
+mod init;
 pub mod installation;
 mod state;
 
 pub use self::cache::Cache as Cache;
+pub use self::init::get_config as get_config;
+pub use self::init::get_cache as get_cache;
+pub use self::init::get_repositories as get_repositories;
 pub use self::state::PackageState as PackageState;
