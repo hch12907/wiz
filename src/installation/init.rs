fdc36bdd673b0913dd6e42dcb2d752186afc6939
hch12907 <haochengho12907@gmail.com>
2017-12-15 00:00:54 +0800

    Make functions public

--- src/installation/init.rs
+++ src/installation/init.rs
@@ -1,16 +1,21 @@
-use std::path::Path;
+use std::path::{ Path, PathBuf };
 
 use config::Config;
 use installation::{ Cache, PackageState };
+use repository::RepositoryList;
 
 /// Gets the config, fill it with default values, and return it.
-fn get_config<P: AsRef<Path>>(path: P) -> Config {
+pub fn get_config<P: AsRef<Path>>(path: P) -> Config {
     Config::read_from(path)
         .map(|x| x.fill_with_default())
         .unwrap()
 }
 
-/// Gets the cached repositories.
-fn get_cache<P: AsRef<Path>>(path: P) -> Cache {
+/// Gets the list of installed packages.
+pub fn get_cache<P: AsRef<Path>>(path: P) -> Cache {
     Cache::read_from(path).unwrap()
+}
+
+pub fn get_repositories<P: AsRef<Path>>(path: P) -> RepositoryList {
+    RepositoryList::read_from(path).unwrap()
 }
