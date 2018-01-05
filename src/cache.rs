732f5acf53c8254ffc17c068b6aa045cdad224e9
hch12907 <haochengho12907@gmail.com>
2018-01-06 01:04:23 +0800

    Some missing error handlings.

--- src/cache.rs
+++ src/cache.rs
@@ -15,7 +15,7 @@ impl Cache {
         let file = File::open(path)?;
         let mut file = BufReader::new(file);
         let mut content = String::new();
-        file.read_to_string(&mut content);
+        file.read_to_string(&mut content)?;
         let content = content.trim().parse::<Value>()?;
         Ok(content.try_into::<Self>()?)
     }
--- src/repository/list.rs
+++ src/repository/list.rs
@@ -16,7 +16,7 @@ impl RepositoryList {
         let file = File::open(path)?;
         let mut file = BufReader::new(file);
         let mut content = String::new();
-        file.read_to_string(&mut content);
+        file.read_to_string(&mut content)?;
         let content = content.trim().parse::<Value>()?;
         Ok(content.try_into::<Self>()?)
     }
